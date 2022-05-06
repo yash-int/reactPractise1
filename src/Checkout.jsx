ab7f580a68dafc209ff2b4c43ed5c284a89b69a6
yash <yash.garg@indusnet.co.in>
2022-05-06 11:32:10 +0530

    working on subtotal module

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import checkout__ad from "./images/checkout__ad.jpg"
+import React from "react";
+import checkout__ad from "./images/checkout__ad.jpg";
+import "./Checkout.css"
+import Subtotal from "./Subtotal.jsx"
 
 function Checkout() {
   return (
-    <div className='checkout'>
-        <div className="checkout__left">
-            <img height={"200px"} width={"100%"} src={checkout__ad} alt="checkout_ad" className="checkout__ad" />
-            <h2 className='checkout__title'>Your Shopping Basket</h2>
-        </div>
-        <div className="checkout__right">
-            the subtotal will go here
-        </div>
-        <div>
+    <div className="checkout">
 
-        </div>
+      <div className="checkout__left">
+        <img
+          height={"200px"}
+          width={"70%"}
+          src={checkout__ad}
+          alt="checkout_ad"
+          className="checkout__ad"
+        />
+        <h2 className="checkout__title">Your Shopping Basket</h2>
+      </div>
+      <div className="checkout__right">
+         <Subtotal/>
+              </div>
+      
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
